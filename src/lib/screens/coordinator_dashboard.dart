ea87fc3bbe32dcede86919388200782cbe207246
Vishal Gore <emailvishalgore@gmail.com>
2026-06-24 12:29:09 +0530

    Fix compile errors: add missing google_fonts import statement to screen files

--- src/lib/screens/coordinator_dashboard.dart
+++ src/lib/screens/coordinator_dashboard.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import '../app_state.dart';
