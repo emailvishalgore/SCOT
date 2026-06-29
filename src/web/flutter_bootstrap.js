2f855bd3797d56d5451348e49ab473f52eda5b12
Vishal Gore <emailvishalgore@gmail.com>
2026-06-29 12:51:40 +0530

    fix(web): add onEntrypointLoaded handler in flutter_bootstrap.js to initialize and run engine

--- src/web/flutter_bootstrap.js
+++ src/web/flutter_bootstrap.js
@@ -5,4 +5,8 @@ _flutter.loader.load({
   config: {
     renderer: 'html',
   },
+  onEntrypointLoaded: async function(engineInitializer) {
+    const appRunner = await engineInitializer.initializeEngine();
+    await appRunner.runApp();
+  }
 });
